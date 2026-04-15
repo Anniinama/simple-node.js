05c5ceb0a275d6977c5ef9f73f5a6ad71f0dedcb
Anniina <anniina20101@student.hamk.fi>
2026-04-15 14:17:35 +0300

    creating way to get all the legoflowewrs from array

--- index.js
+++ index.js
@@ -8,6 +8,93 @@ require('dotenv').config();
 //app
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//data
+let lego_flowers = [
+    {name: "Sunflower Bouquet",
+    pieces: 686,
+    price: 69.95,
+    category: "Bouquet",
+    colorOfTheFlower: "Yellow",
+    inStock: true
+    },
+    {name: "Magnolia Branches",
+    pieces: 435,
+    price: 59.95,
+    category: "Branches",
+    colorOfTheFlower: "Multicolor",
+    inStock: true
+    },
+    {name: "Tulip Bouquet",
+    pieces: 576,
+    price: 69.95,
+    category: "Bouquet",
+    colorOfTheFlower: "Multicolor",
+    inStock: false
+    },
+    {name: "Bouquet of Pink Roses",
+    pieces: 789,
+    price: 69.95,
+    category: "Bouquet",
+    colorOfTheFlower: "Pink",
+    inStock: true
+    },
+    {name: "Orchid",
+    pieces: 608,
+    price: 59.95,
+    category: "Potted plant",
+    colorOfTheFlower: "White",
+    inStock: true
+    },
+    {name: "Wildflower Bouquet",
+    pieces: 939,
+    price: 74.95,
+    category: "Bouquet",
+    colorOfTheFlower: "Multicolor",
+    inStock: true
+    },
+    {name: "Cherry Blossoms",
+    pieces: 430,
+    price: 17.95,
+    category: "Branches",
+    colorOfTheFlower: "Multicolor",
+    inStock: true
+    },
+    {name: "Daisies",
+    pieces: 133,
+    price: 17.95,
+    category: "Bouquet",
+    colorOfTheFlower: "White",
+    inStock: false
+    },
+    {name: "Pretty Pink Flower Bouquet",
+    pieces: 749,
+    price: 72.95,
+    category: "Bouquet",
+    colorOfTheFlower: "Multicolor",
+    inStock: true
+    },
+    {name: "Bouquet of Roses",
+    pieces: 822,
+    price: 72.95,
+    category: "Bouquet",
+    colorOfTheFlower: "Red",
+    inStock: true
+    }
+]
+
+//getting all legoflowers from array
+app.get('/api/lego_flowers', (req,res) => {
+    res.status(200).json({
+        status: 'Success',
+        results: lego_flowers.length,
+        data: lego_flowers
+    })
+});
+
+
 //port
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
