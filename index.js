08419c507c8196e4c750f9695480055d4dbd0358
Anniina <anniina20101@student.hamk.fi>
2026-04-15 13:57:02 +0300

    added .env and index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-node_modules
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+//how to import env
+require('dotenv').config();
+
+//app
+const app = express();
+
+//port
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
